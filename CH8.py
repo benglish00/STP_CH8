f73ecb2d421d333f787971e05d8dc253e00e4ca9
benglish00 <benglish00@users.noreply.github.com>
2017-11-02 22:51:38 -0600

    add challenges.

--- CH8.py
+++ CH8.py
@@ -3,6 +3,7 @@ import random
 import statistics
 import keyword
 import hello
+import cubed
 
 # use code from the module by [module name].code
 print(math.pow(2,3))
@@ -14,3 +15,8 @@ print(statistics.stdev(nums))
 keyword.iskeyword("for")
 keyword.iskeyword("football")
 hello.print_hello()
+x0 = cubed.cube(nums[0])
+x1 = cubed.cube(nums[1])
+x2 = cubed.cube(nums[2])
+
+print(x0, x1, x2)
